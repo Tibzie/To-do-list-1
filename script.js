b3b18007269685384527d58c7357a06fa1ddcb64
Tibor Gazsovics <gt008@freemail.hu>
2021-03-04 19:00:44 +0000

    finishing touches

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 let input = document.getElementById("input");
 let btn = document.getElementById("cta-btn");
 let list = document.getElementById("list");
+let taskCount = document.getElementById("task-count");
+
+// btn.addEventListener("click", (item, remove, edit) => {
+//     item = document.createElement("li");
+//     item.innerHTML = input.value;
+//     list.appendChild(item);
+//     remove = document.createElement("button");
+//     remove.innerHTML = "x";
+//     item.appendChild(remove);
+//     remove.addEventListener("click", () => {
+//         list.removeChild(item);
+//     });
+//     input.value = "";
+// });
 
 btn.addEventListener("click", (item, remove, edit) => {
     item = document.createElement("li");
     item.innerHTML = input.value;
     list.appendChild(item);
-    remove = document.createElement("button");
-    remove.innerHTML = "x";
-    item.appendChild(remove);
-    remove.addEventListener("click", () => {
+    item.addEventListener("click", () => {
         list.removeChild(item);
     });
     input.value = "";

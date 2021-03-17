33cd32845d9de700df848f3500788022bfc9093d
Tibor Gazsovics <gazsovicst@gmail.com>
2021-03-17 17:00:38 +0000

    bugfix #1: fixed long li elements

--- script.js
+++ script.js
@@ -3,19 +3,6 @@ let btn = document.getElementById("cta-btn");
 let list = document.getElementById("list");
 let taskCount = document.getElementById("task-count");
 
-// btn.addEventListener("click", (item, remove, edit) => {
-//     item = document.createElement("li");
-//     item.innerHTML = input.value;
-//     list.appendChild(item);
-//     remove = document.createElement("button");
-//     remove.innerHTML = "x";
-//     item.appendChild(remove);
-//     remove.addEventListener("click", () => {
-//         list.removeChild(item);
-//     });
-//     input.value = "";
-// });
-
 btn.addEventListener("click", (item) => {
     item = document.createElement("li");
     item.innerHTML = input.value;
